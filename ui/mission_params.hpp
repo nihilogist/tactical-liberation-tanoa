e05d486bdb1aa83e4fe8b2c3cfb020e442affc4b
David Heaton <dave3.heaton@gmail.com>
2019-11-13 23:40:46 +0000

    Update vehicle cleanup to 4hrs

--- ui/mission_params.hpp
+++ ui/mission_params.hpp
@@ -304,7 +304,7 @@ class Params
         title = $STR_CLEANUP_PARAM;
         values[] = { 0,1,2,4 };
         texts[] = { $STR_PARAMS_DISABLED, $STR_CLEANUP_PARAM1, $STR_CLEANUP_PARAM2, $STR_CLEANUP_PARAM3 };
-        default = 2;
+        default = 4;
     };
     class Introduction {
         title = $STR_PARAMS_INTRO;
